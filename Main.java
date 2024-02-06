e51c4b82b474e9f144465e8d2a6df76a5ee9e836
Khaled Mousa <Khaledmousa216@gmail.com>
2024-02-05 23:28:15 -0500

    2nd commit

--- Main.java
+++ Main.java
@@ -1,18 +1,5 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 public class Main {
   public static void main(String[] args) {
-    JLabel label = new JLabel(); 
-    label.setText("GPA Calc"); 
-    label.setHorizontalAlignment(JLabel.CENTER);     
-    
-    JFrame frame = new JFrame(); 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-    frame.add(label); 
-    frame.pack(); 
-    frame.setSize(400,600);
-    frame.setLocationRelativeTo(null); 
-    frame.setVisible(true); 
+    new GUI() ;
   }
 }
