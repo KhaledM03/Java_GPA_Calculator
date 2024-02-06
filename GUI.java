0d8464505bf2197d05ed702cd5e6a32b7e1b4eda
Khaled Mousa <Khaledmousa216@gmail.com>
2024-02-06 00:25:34 -0500

    added button and set a panel

--- GUI.java
+++ GUI.java
@@ -1,20 +1,33 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GUI {
+   private JLabel label ; 
+   private JFrame frame ; 
+   private JPanel panel ; 
+   private JButton button; 
+
 
    public  GUI(){
-    JLabel label = new JLabel(); 
-    label.setText("GPA Calc"); 
-    label.setHorizontalAlignment(JLabel.CENTER);     
-    
-    JFrame frame = new JFrame(); 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-    frame.add(label); 
-    frame.pack(); 
-    frame.setSize(400,600);
-    frame.setLocationRelativeTo(null); 
-    frame.setVisible(true); 
-   }
+   frame = new JFrame() ; 
+   button = new JButton() ;
+   label = new JLabel("GPA Calc"); 
    
+   label.setHorizontalAlignment(JLabel.CENTER);     
+    
+   panel = new JPanel() ; 
+   panel.add(label) ; 
+   panel.add(button); 
+   button = new JButton("Submit") ;
+
+   frame.add(panel); 
+   
+   frame.setSize(400,600);
+   frame.setLocationRelativeTo(null); 
+   frame.setVisible(true); 
+   } 
 }
